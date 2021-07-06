bac4b91ccaed04c69c2d4dc096cc24bb1b1ece42
Cleyson Lima <cleysonph@gmail.com>
2021-07-06 00:40:19 -0300

    04:04 - adiciona o Spring Security

---
+++ src/main/java/br/com/treinaweb/javajobs/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package br.com.treinaweb.javajobs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String API_JOBS_URL = "/api/v1/jobs/**";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+            .disable();
+
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.GET, API_JOBS_URL).permitAll()
+            .anyRequest().authenticated();
+
+        http.sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}
