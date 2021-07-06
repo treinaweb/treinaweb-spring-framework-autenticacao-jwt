478e3c57bb58757c5f1065d00879b1f9ce411f3c
Cleyson Lima <cleysonph@gmail.com>
2021-07-06 00:48:25 -0300

    04:05 - cria entidade User e o seu repositorio

---
+++ src/main/java/br/com/treinaweb/javajobs/models/User.java
@@ -0,0 +1,32 @@
+package br.com.treinaweb.javajobs.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
